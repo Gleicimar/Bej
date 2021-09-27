ba6131be4d15f0145ce65ad114196ec852e5dd91
Gleicimar <gleicimarribeiro@gmail.com>
2021-09-26 23:33:04 -0300

    Add files via upload
    atualizado  js e css

---
+++ js/functions.js
@@ -0,0 +1,7 @@
+const btnMobile =document.getElementById('btn');
+
+function toggleMenu( ){
+const nav =document.getElementById('nav');
+nav.classList.toggle('active');
+}
+btnMobile.addEventListener('click',toggleMenu);
